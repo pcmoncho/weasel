7cf33491b1a8bca7d1a0086143db94edee968499
Pedro Cisternas <pedro.cisternas_ex@scotiabank.cl>
2018-03-20 11:34:35 -0300

    CORS

--- index.js
+++ index.js
@@ -10,21 +10,43 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+const fs = require('fs');
+const cors = require('cors');
 
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(bodyParser.json());
 app.use(methodOverride());
-app.use(express.static('dist'));
 
 const router = express.Router();
 const rests = express.Router();
 
-resources.forEach((resource)=> rests.route(resource.path).all((req, res)=> res.status(resource.status).jsonp(resource.response)));
+app.use((req, res, next)=> {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'content-type', 'application/json');
 
-//router.get('/', (req, res) => res.send("Aquí no hay nada"));
-app.get('/', (req, res) => res.sendFile(`${__dirname} /dist/index.html`));
+    next();
+});
+
+app.use(cors());
+
+resources.forEach((resource)=> {
+    if(resource.response.hasOwnProperty('weaselDocPath')) convertPDFToByteArray(resource);
+    rests.route(resource.path).all((req, res)=> res.status(resource.status).jsonp(resource.response));
+});
+
+function convertPDFToByteArray(resource){
+    rests.route(resource.path).all((req, res)=>{
+        console.log(resource.response.weaselDocPath);
+        fs.readFile(resource.response.weaselDocPath, "utf8", (err, data)=> {
+            res.status(resource.status).jsonp(data);
+        });
+    });
+}
+
+router.get('/', (req, res) => res.send("Aquí no hay nada"));
 
 app.use(rests);
 app.use(router);
